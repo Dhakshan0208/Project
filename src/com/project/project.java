b8e4ab7c85fbd095f54c514d201c48708614e86f
prabh <prabh@Dhakshna>
2023-10-02 13:40:39 +0530

    commiting

---
+++ src/com/project/project.java
@@ -0,0 +1,15 @@
+package com.project;
+
+
+
+public class project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				
+		System.out.println("hello");	
+
+	}
+
+}
